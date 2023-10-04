5c450b29c94ebfda02a3124e276f85afec4469b2
Ivan Knysh <mr.rauber.ik@gmail.com>
2023-10-04 20:02:56 +0300

    Added fixed scroll header

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 "use strict";
 
 // Lightbox settings
-new SimpleLightbox(".gallery a", {
-	overlayOpacity: 0.8,
-	scrollZoom: false,
+if (document.querySelector(".gallery")) {
+	new SimpleLightbox(".gallery a", {
+		overlayOpacity: 0.8,
+		scrollZoom: false,
+	});
+}
+
+// Nav menu
+window.addEventListener("scroll", (e) => {
+	if (window.scrollY > 120) {
+		document.querySelector(".header").classList.add("active-header");
+	} else {
+		document.querySelector(".header").classList.remove("active-header");
+	}
 });
